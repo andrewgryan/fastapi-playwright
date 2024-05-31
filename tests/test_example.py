8e607ba8349cda577fe16e34dbdfe234846db7d9
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-05-31 11:41:02 +0100

    include start/stop server in the test suite

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,23 @@
-import re
+from multiprocessing import Process
+import uvicorn
+import pytest
 from playwright.sync_api import Page, expect
+from fastapi_vitest.main import app
 
-def test_htmx_org_example(page: Page):
+
+def run_server():
+    uvicorn.run(app)
+
+
+@pytest.fixture(scope="session")
+def server():
+    proc = Process(target=run_server, args=(), daemon=True)
+    proc.start() 
+    yield
+    proc.kill() # Cleanup after test
+
+
+def test_htmx_org_example(page: Page, server):
     page.goto("http://127.0.0.1:8000/")
     page.get_by_role("button").click()
     expect(page.get_by_role("paragraph")).to_be_visible()
