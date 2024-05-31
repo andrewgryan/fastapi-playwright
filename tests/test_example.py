19ac29a9a85c65b2c86e83763c443b161fe0be8d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-05-31 10:44:30 +0100

    rename test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,7 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
+def test_htmx_org_example(page: Page):
     page.goto("http://127.0.0.1:8000/")
-    page.on("console", lambda msg: print(msg.text))
     page.get_by_role("button").click()
     expect(page.get_by_role("paragraph")).to_be_visible()
